502251a6e4647f16e67d770024c185b3e8cb9d98
r-bhanu <83873563+r-bhanu@users.noreply.github.com>
2022-07-07 16:33:44 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,8 +14,8 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://bhanu:br1603@cluster0.h1q2rse.mongodb.net/?retryWrites=true&w=majority';
-// const CONNECTION_URL = 'mongodb://localhost:27017/social';
+// const CONNECTION_URL = 'mongodb+srv://bhanu:<password>@cluster0.h1q2rse.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb://localhost:27017/social';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
